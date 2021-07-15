32f5c4cb9a434b2a402c676383483bad0fcf372e
naruhide.ueda <neruneru.2958.un@gmail.com>
2021-07-15 19:26:20 +0900

    Add cloud9 directory

--- main.tf
+++ main.tf
@@ -1,6 +1,3 @@
-# ---------------------------------------------
-# Terraform configuration
-# ---------------------------------------------
 terraform {
   required_version = "=1.0.2"
   required_providers {
@@ -11,10 +8,11 @@ terraform {
   }
 }
 
-# ---------------------------------------------
-# Provider
-# ---------------------------------------------
 provider "aws" {
   profile = "terraform"
   region  = "ap-northeast-1"
+}
+
+module "cloud9" {
+  source = "./cloud9"
 }
